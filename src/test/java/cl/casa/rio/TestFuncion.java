05fe367c8d82ddf91bd0a52de4db823f7dd9f00b
Victor Eduardo Carreño Caces <binomioalcubo@gmail.com>
2025-09-14 23:19:54 -0300

    fiz

--- src/test/java/cl/casa/rio/TestFuncion.java
+++ src/test/java/cl/casa/rio/TestFuncion.java
@@ -1,4 +1,33 @@
 package cl.casa.rio;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.example.Funcion;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestFuncion {
+
+
+
+    private int numero;
+    private int resultado;
+
+
+    @Given("El numero {int}")
+    public void definirNumero(int numweo){
+        this.numero = numweo;
+
+    }
+    @When("ejecuto el metodo factorial")
+   public void asignarResultado(){
+        Funcion funcion = new Funcion();
+        resultado = funcion.factorial(numero);
+    }
+    @Then("el resultado debe ser {int}")
+    public void resukltadoEsperado(int esperado){
+        assertEquals(esperado, resultado);
+
+    }
 }
